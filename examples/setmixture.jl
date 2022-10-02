d7dde48cdc127cbd6d4d4a5386b3bf9538e222b7
Martin Rektoris <martin.rektoris@gmail.com>
2022-10-02 18:22:34 +0200

    Add data preprocessing and result evaluation

--- examples/setmixture.jl
+++ examples/setmixture.jl
@@ -4,9 +4,10 @@ using SumProductSet
 using Plots
 using Flux
 using StatsBase
+using Clustering
 import Mill
 
-function train!(m::SumNode, x::Mill.BagNode; niter::Int=400, opt=ADAM(0.02))
+function train!(m::SumNode, x::Mill.BagNode; niter::Int=200, opt=ADAM(0.02))
     ps = Flux.params(m)
 
     for i in 1:niter
@@ -21,24 +22,32 @@ end
 Σ = ([3. -1.6; -1.6 3], [3. 1.6; 1.6 6], [3. 1.6; 1.6 6])
 λ = (26, 26, 6)
 
-function knownsetmixture(μs, Σs, λs)
+function knownsetmixture(μs, Σs, λs, prior)
     components = map(zip(μs, Σs, λs)) do ps
         pc = _Poisson(log(ps[3]))
         pf = _MvNormalParams(ps[1], ps[2])
         SetNode(pf, pc)
     end
-    SumNode(components)    
+    SumNode(components, prior)    
 end
-m1 = knownsetmixture(μ, Σ, λ)
+m1 = knownsetmixture(μ, Σ, λ, [1., 1., 1.])
 nbags = 300
 bags, baglabels = randwithlabel(m1, nbags)
 
 instances = bags.data.data
 bagids = bags.bags
 
-instlabels = mapreduce(a -> repeat([a[1]], length(a[2])), vcat, zip(baglabels, bagids))
-display(scatter(instances[1, :], instances[2, :], group=instlabels, alpha=0.8, title="Instance space"))
+mn = mean(instances, dims=2)
+sd =  std(instances, dims=2)
+instances = (instances .- mn) ./ sd
 
-println("Reference average loglikelihood: $(mean(logpdf(m1, bags)))")
-m2 = setmixture(2, 1, 2; fdist=:MvNormal)
-train!(m2, bags)
+stdbags = Mill.BagNode(Mill.ArrayNode(instances), bagids)
+
+instlabels = mapreduce(a -> repeat([a[1]], length(a[2])), vcat, zip(baglabels, bagids))
+display(scatter(instances[1, :], instances[2, :], group=instlabels, alpha=0.8, title="Normalized innstance space"))
+
+m2 = setmixture(3, 1, 2; fdist=:MvNormal)
+train!(m2, stdbags)
+clusters = mapslices(argmax, logjnt(m2, stdbags), dims=1)[:]
+ari = randindex(baglabels, clusters)[1]
+println("Achieved ARI: $(ari) on train set.")
