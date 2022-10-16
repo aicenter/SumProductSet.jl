ede7907f4c9e9f55cabde03eb8dd54a7e53d7ade
Martin Rektoris <martin.rektoris@gmail.com>
2022-10-16 23:17:07 +0200

    Add set mixture with shared instance nodes

--- src/modelbuilders.jl
+++ src/modelbuilders.jl
@@ -19,3 +19,17 @@ function setmixture(nb::Int, ni::Int, d::Int;
     end
     SumNode(components; dtype=dtype)
 end
+
+function sharedsetmixture(nb::Int, nis::Int, nin::Int, d::Int;
+    dtype::Type{<:Real}=Float64, μinit::Symbol=:uniform, Σinit::Symbol=:unit, Σtype::Symbol=:full)
+    ps = (; dtype, μinit, Σinit, Σtype)
+
+    sharedcomps = [_MvNormal(d; ps...) for _ in 1:nis]
+    components = map(1:nb) do _
+        pc = _Poisson()
+        nonsharedcomps = [_MvNormal(d; ps...) for _ in 1:nin]
+        pf = SumNode([nonsharedcomps; sharedcomps]; dtype)
+        SetNode(pf, pc)
+    end
+    SumNode(components; dtype)
+end
