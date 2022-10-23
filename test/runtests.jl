9f79955991e002789edf007203dfe3b49e536f19
Martin Rektoris <martin.rektoris@gmail.com>
2022-10-23 18:59:17 +0200

    Include test in runtest.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,1 +1,8 @@
 using SumProductSet, Test, Distributions
+
+include("distributions/mvnormal.jl")
+include("distributions/poisson.jl")
+include("distributions/categorical.jl")
+
+include("setnode.jl")
+include("modelbuilders.jl")
