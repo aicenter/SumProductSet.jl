47f298b1810d3af4c0e8c7a2ce62dc9b6aec39af
Martin Rektoris <martin.rektoris@gmail.com>
2022-11-03 10:30:31 +0100

    Refactor _Poisson a bit
    Restrict _Poisson to univariate case
    Extend logpdf to Vector inputs
    Extend rand to sample multiple values at the same time

--- src/distributions/poisson.jl
+++ src/distributions/poisson.jl
@@ -1,6 +1,10 @@
 
 mutable struct _Poisson{T} <: _Distribution{T}
     logλ::Array{T, 1}
+    function _Poisson(logλ::Array{T, 1}) where T
+        @assert length(logλ) == 1
+        new{T}(logλ)
+    end
 end
 Flux.@functor _Poisson
 
@@ -8,11 +12,12 @@ _Poisson(logλ::Real) = _Poisson([logλ])
 _Poisson(logλ::Integer) = _Poisson(Float64(logλ))
 _Poisson() = _Poisson(log(rand(2:5)))
 
-Base.rand(m::_Poisson) = length(m.logλ) > 1 ? throw(error("rand(m::_Poisson) is not defined for length(m.logλ) > 1")) : pois_rand(exp(m.logλ[1]))
+Base.rand(m::_Poisson) = pois_rand(exp(m.logλ[1]))
+Base.rand(m::_Poisson, n::Int) = map(rand(m), 1:n)
 Base.length(m::_Poisson) = length(m.logλ)
 
 _poisson_logpdf(logλ, x) = x .* logλ .- exp(logλ) .- logfactorial.(x)
 
-function logpdf(m::_Poisson, x::Real)
+function logpdf(m::_Poisson, x::Union{Real, Vector{<:Real}})
     mapreduce(logλ -> _poisson_logpdf(logλ, x), +, m.logλ)
 end
