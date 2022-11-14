51fe75736880c6c68f0f74705e42ca9f7e245d98
Martin Rektoris <martin.rektoris@gmail.com>
2022-11-14 20:23:43 +0100

    Create new slurm scripts

--- scripts/run_slurm.sh
+++ scripts/run_slurm.sh
@@ -1,5 +1,54 @@
 #!/bin/bash
 
-for d in {1..20}; do
-    sbatch scripts/real.jl $d
-done
+ndatasets=20
+ngrid=150
+max_jobs=400
+start=1
+
+
+function run() {
+    read unavail <<< $(
+    squeue --user=$USER "$@" | awk '
+    BEGIN {
+        abbrev["R"]="(Running)"
+        abbrev["PD"]="(Pending)"
+        abbrev["CG"]="(Completing)"
+        abbrev["F"]="(Failed)"
+        unvail=0
+    }
+    NR>1 {a[$5]++}
+    END {
+        for (i in a) {
+            # printf "%-2s %-12s %d\n", i, abbrev[i], a[i]
+            unavail=unavail+a[i]
+        }
+        printf "%d\n", unavail
+    }'
+    )
+    echo $unavail;
+
+    avail=$max_jobs-$((unavail))
+    if [ $avail -gt 0]; then
+        end=$start + $avail
+    else
+        end=0
+    fi 
+    
+    counter=1
+    for c in {1..$ndatasets}; do
+        for d in {1..$ngrid}; do
+            if (( $counter > $start && $counter <= $end )); then
+                sbatch scripts/hmill_classifier.jl $c $d
+            fi
+            counter=$counter+1
+
+            if [$counter -gt $end]; then
+                start = $end
+                export start
+            fi
+        done
+    done
+}
+
+# run in 2 minute intervals
+watch -n 120 run -r
