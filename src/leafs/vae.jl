992caa078731e4bcb0f2df65703de75d33a366d7
Martin Rektoris <martin.rektoris@gmail.com>
2022-11-17 17:18:26 +0100

    Fix vae conditional loglikelihood

--- src/leafs/vae.jl
+++ src/leafs/vae.jl
@@ -43,20 +43,20 @@ end
 # KL divergence between N(μ, diag(σ^2)) and N(0, I)
 kl_loss(μ::Array{T, 2}, σ::Array{T, 2}) where T = T(0.5) .* sum( σ.^2 .+ μ.^2 .- T(1) .- T(2).*log.(σ), dims=1)
 
-function reconstruct_loss(x::Array{T, 2}, x̂::Array{T, 2}; σ::T=T(0.1)) where T 
+function reconstruct_loss(x::AbstractArray{T}, x̂::AbstractArray{T}; σ::T=T(0.5)) where T 
     k = size(x, 1)
     y = (x .- x̂) ./ σ
-    -T(k/2) * ( T(2*pi) + log(σ)) .- T(0.5)*sum( y.^2, dims=1)
+    -T(k/2) * ( T(log(2*pi)) + 2*log(σ)) .- T(0.5)*sum( y.^2, dims=1)
 end
 
-function elbo(m::VAE, x::AbstractArray{T}) where T
+function elbo(m::VAE, x::AbstractArray{T}; σd=0.1) where T
     
     # sample latent variable
     μ, σ = m.encoder(x) # encoder contains split layer
     # reparametrisation trick
     z = μ .+ σ .* randn(T, size(μ)...)
 
-    rec = reconstruct_loss(x, m.decoder(z))
+    rec = reconstruct_loss(x, m.decoder(z); σ=σd)
     kl = kl_loss(μ, σ)
     mean(rec - kl)
 end
