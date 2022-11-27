4225cc0a8c27ae901d6114b3c8b1528f89dbdbdb
Martin Rektoris <martin.rektoris@gmail.com>
2022-11-27 15:39:29 +0100

    Enable regularization in modelbuilders

--- src/modelbuilders.jl
+++ src/modelbuilders.jl
@@ -1,15 +1,15 @@
 
 function gmm(n::Int, d::Int; 
-    dtype::Type{<:Real}=Float64, μinit::Symbol=:uniform, Σinit::Symbol=:unit, Σtype::Symbol=:full)
+    dtype::Type{<:Real}=Float64, μinit::Symbol=:uniform, Σinit::Symbol=:unit, Σtype::Symbol=:full, r::Real=0.)
 
-    ps = (; dtype, μinit, Σinit, Σtype)
+    ps = (; dtype, μinit, Σinit, Σtype, r)
     SumNode([_MvNormal(d; ps...) for _ in 1:n]; dtype=dtype)
 end
 
 function setmixture(nb::Int, ni::Int, d::Int; cdist::Function=()->_Poisson(),
-    dtype::Type{<:Real}=Float64, μinit::Symbol=:uniform, Σinit::Symbol=:unit, Σtype::Symbol=:full)
+    dtype::Type{<:Real}=Float64, μinit::Symbol=:uniform, Σinit::Symbol=:unit, Σtype::Symbol=:full, r::Real=0.)
 
-    ps = (; dtype, μinit, Σinit, Σtype)
+    ps = (; dtype, μinit, Σinit, Σtype, r)
     fdist() = ni > 1 ? gmm(ni, d; ps...) : _MvNormal(d; ps...)
     
     components = map(1:nb) do _
@@ -21,8 +21,8 @@ function setmixture(nb::Int, ni::Int, d::Int; cdist::Function=()->_Poisson(),
 end
 
 function sharedsetmixture(nb::Int, nis::Int, nin::Int, d::Int; cdist::Function=()->_Poisson(), 
-    dtype::Type{<:Real}=Float64, μinit::Symbol=:uniform, Σinit::Symbol=:unit, Σtype::Symbol=:full)
-    ps = (; dtype, μinit, Σinit, Σtype)
+    dtype::Type{<:Real}=Float64, μinit::Symbol=:uniform, Σinit::Symbol=:unit, Σtype::Symbol=:full, r::Real=0.)
+    ps = (; dtype, μinit, Σinit, Σtype, r)
 
     sharedcomps = [_MvNormal(d; ps...) for _ in 1:nis]
     bagcomps = map(1:nb) do _
