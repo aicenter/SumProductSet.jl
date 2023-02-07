ed5a923251f9b955ca42f9ec26a4f98538274b8b
Martin Rektoris <martin.rektoris@gmail.com>
2023-02-07 18:55:04 +0800

    Update reflectinmodel

--- src/reflectinmodel.jl
+++ src/reflectinmodel.jl
@@ -1,27 +1,29 @@
 """
-    x - one Mill sample
-    n - number of classes/cluster
-    fm - leaf distribution for continuous data. 
-        Should be function d -> dist(d), whered is dimenion of feature vector in leaves.
+    Build model mixture model of HMILL data.
+    x - one HMLL sample
+    n - number of mixture components in root node 
+    f_{type} - leaf distribution for {type} variable
+        Should be function d -> dist(d), where d is leaf data dimension
 """
-function reflectinmodel(x, n::Int, fm=d->gmm(2, d;), cdist=()->_Poisson(); dtype::Type{<:Real}=Float64)
-    SumNode([_reflectinmodel(x, fm, cdist; dtype) for _ in 1:n]; dtype=dtype)
+
+function reflectinmodel(x, n::Int; f_cont=d->gmm(2, d), f_cat=d->_Categorical(d), f_card=()->_Poisson(), dtype::Type{<:Real}=Float64)
+    SumNode([_reflectinmodel(x; f_cont=f_cont, f_cat=f_cat, f_card=f_card, dtype=dtype) for _ in 1:n]; dtype=dtype)
 end
 
-function _reflectinmodel(x::Mill.BagNode, fm, cdist; dtype)
-    fdist = _reflectinmodel(x.data, fm, cdist; dtype)
-    SetNode(fdist, cdist())
+function _reflectinmodel(x::Mill.BagNode; f_cont, f_cat, f_card, dtype)
+    fdist = _reflectinmodel(x.data; f_cont=f_cont, f_cat=f_cat, f_card=f_card, dtype=dtype)
+    SetNode(fdist, f_card())
 end
 
-function _reflectinmodel(x::Mill.ProductNode, fm, cdist; dtype)
+function _reflectinmodel(x::Mill.ProductNode{<:NamedTuple}; f_cont, f_cat, f_card, dtype)
     ks = keys(x.data)
-    m = tuple([_reflectinmodel(x.data[k], fm, cdist; dtype) for (i, k) in enumerate(ks)]...)
-    ProductNode(m)
+    ms = NamedTuple(k => _reflectinmodel(x.data[k]; f_cont=f_cont, f_cat=f_cat, f_card=f_card, dtype=dtype) for k in ks)
+    ProductNode(ms)
 end
 
-_reflectinmodel(x::Mill.ArrayNode, fm, cdist; dtype) = _reflectinmodel(x.data, fm, cdist; dtype)
-_reflectinmodel(x::Flux.OneHotArray, fm, cdist; dtype) = _Categorical(size(x, 1))
-_reflectinmodel(x::AbstractArray, fm, cdist; dtype) = fm(size(x, 1))
+_reflectinmodel(x::Mill.ArrayNode; f_cont, f_cat, f_card, dtype) = _reflectinmodel(x.data; f_cont=f_cont, f_cat=f_cat, f_card=f_card, dtype=dtype)
+_reflectinmodel(x::Flux.OneHotArray; f_cont, f_cat, f_card, dtype) = f_cat(size(x, 1))
+_reflectinmodel(x::AbstractArray; f_cont, f_cat, f_card, dtype) = f_cont(size(x, 1))
 
 
 
