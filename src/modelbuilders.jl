fdf11eaf78f5ad5381cd8da1f12bec53d9be9577
Martin Rektoris <martin.rektoris@gmail.com>
2023-02-12 00:10:21 +0800

    Add binary split spn builder

--- src/modelbuilders.jl
+++ src/modelbuilders.jl
@@ -6,6 +6,36 @@ function gmm(n::Int, d::Int;
     SumNode([_MvNormal(d; ps...) for _ in 1:n]; dtype=dtype)
 end
 
+function spn(d::Int, l::Int, n::Int)
+    n == 1 && l == 1 && return _MvNormal(d)
+    l == 1 && return SumNode(map(_->_MvNormal(d), 1:n)...)
+
+    n == 1 && d == 1 && return _MvNormal(d)
+    d == 1 && return SumNode(map(_->_MvNormal(d), 1:n)...)
+    
+    r = ceil(Int, d / 2)
+    comp_sum = map(1:n) do _
+        comp_prod = [spn(r, l-1, n), spn(d-r, l-1, n)]
+        ProductNode(comp_prod...)
+    end
+    length(comp_sum) == 1 ? first(comp_sum) : SumNode(comp_sum...)
+end
+
+function spn(d::Int, n::Vector{Int})
+    length(n) == 1 && n[1] == 1 && return _MvNormal(d)
+    length(n) == 1 && return SumNode(map(_->_MvNormal(d), 1:n[1])...)
+
+    d == 1 && n[1] == 1 && return _MvNormal(d)
+    d == 1 && return SumNode(map(_->_MvNormal(d), 1:n[1])...)
+
+    comp_sum = map(1:n[1]) do _
+        r = ceil(Int, d / 2)
+        comp_prod = [spn(r, n[2:end]), spn(d-r, n[2:end])]
+        length(comp_prod) == 1 ? first(comp_prod) : ProductNode(comp_prod...)
+    end
+    length(comp_sum) == 1 ? first(comp_sum) : SumNode(comp_sum...)
+end
+
 function setmixture(nb::Int, ni::Int, d::Int; cdist::Function=()->_Poisson(),
     dtype::Type{<:Real}=Float64, μinit::Symbol=:uniform, Σinit::Symbol=:unit, Σtype::Symbol=:full, r::Real=0.)
 
