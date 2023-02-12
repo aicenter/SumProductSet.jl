bee87abef4534d055897a43006345da1014fa349
Martin Rektoris <martin.rektoris@gmail.com>
2023-02-12 11:12:49 +0800

    Add Mill set indexing support

--- src/util.jl
+++ src/util.jl
@@ -4,3 +4,6 @@ NODES = Union{SetNode, ProductNode, SumNode}
 logpdf(m::NODES, x::AbstractVector{<:Mill.AbstractMillNode}) = Flux.ChainRulesCore.ignore_derivatives() do
     reduce(Mill.catobs, x)
 end |> xr -> logpdf(m, xr)
+
+Mill.getindex(x::Mill.ProductNode{<:NamedTuple}, k::Union{Vector{Symbol}, NTuple{N, Symbol}}) where N = Mill.ProductNode(x.data[k])
+Mill.getindex(x::Mill.ProductNode{<:NamedTuple}, k::Set{Symbol}) = Mill.getindex(x, collect(k))
