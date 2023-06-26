143e4e9323d06296edec04af528e5e6069e0dc73
Martin Rektoris <martin.rektoris@gmail.com>
2023-06-26 16:01:15 +0200

    Fix Geometric dist _logpdf

--- src/distributions/geometric.jl
+++ src/distributions/geometric.jl
@@ -11,7 +11,13 @@ Geometric(n::Int; dtype::Type{<:Real}=Float32) = Geometric(zeros(dtype, n))
 #   Functions for calculating the likelihood
 ####
 
-_logpdf(m::Geometric, k) = SparseMatrixCSC(k).*log1p.(exp.(m.logp)) .+ m.logp
+# naive implementation of _logpdf
+# function _logpdf(m::Geometric, k) 
+#     p = 1 ./ (1 .+ exp.(-m.logp))
+#     SparseMatrixCSC(k).* log.(1 .- p) .+ log.(p)
+# end
+
+_logpdf(m::Geometric, k) = -SparseMatrixCSC(k).*log1p.(exp.(m.logp)) .- log1p.(exp.(-m.logp))
 
 logpdf(m::Geometric,     x::NGramMatrix{T})         where {T<:Sequence}            = mean(_logpdf(m, x); dims=1)
 logpdf(m::Geometric{Tm}, x::NGramMatrix{Maybe{Tx}}) where {Tm<:Real, Tx<:Sequence} = mean(coalesce.(_logpdf(m, x), Tm(0e0)); dims=1)
