eef312d9b1123fb3289812d4e5fb0ee0259bcde6
Martin Rektoris <martin.rektoris@gmail.com>
2023-07-18 16:10:00 +0200

    Clean up utils

--- src/util.jl
+++ src/util.jl
@@ -1,9 +1,8 @@
 
-NODES = Union{SetNode, ProductNode, SumNode}
+const NODES = Union{SetNode, ProductNode, SumNode}
 
 logpdf(m::NODES, x::AbstractVector{<:Mill.AbstractMillNode}) = Flux.ChainRulesCore.ignore_derivatives() do
     reduce(Mill.catobs, x)
 end |> xr -> logpdf(m, xr)
 
-Mill.getindex(x::Mill.ProductNode{<:NamedTuple}, k::Union{Vector{Symbol}, NTuple{N, Symbol}}) where N = Mill.ProductNode(x.data[k])
-Mill.getindex(x::Mill.ProductNode{<:NamedTuple}, k::Set{Symbol}) = Flux.ChainRulesCore.@ignore_derivatives Mill.getindex(x, collect(k))
+Mill.getindex(x::Mill.ProductNode{<:NamedTuple}, k::Vector{Symbol})= Mill.ProductNode(x.data[k])
