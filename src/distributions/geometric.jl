54de0ad5c5c5366da642fc2053935cb2bf546718
Martin Rektoris <martin.rektoris@gmail.com>
2023-07-24 16:34:09 +0200

    Adjust Geometric logpdf and sampling

--- src/distributions/geometric.jl
+++ src/distributions/geometric.jl
@@ -11,23 +11,18 @@ Geometric(n::Int; dtype::Type{<:Real}=Float32) = Geometric(dtype(0.01)*randn(dty
 #   Functions for calculating the likelihood
 ####
 
-# naive implementation of _logpdf
-# function _logpdf(m::Geometric, k) 
-#     p = 1 ./ (1 .+ exp.(-m.logp))
-#     SparseMatrixCSC(k).* log.(1 .- p) .+ log.(p)
-# end
-
 _logpdf(m::Geometric, k) = _logpdf(m, SparseMatrixCSC(k))
 _logpdf(m::Geometric, k::SparseMatrixCSC) = k .*logsigmoid.(-m.logitp) .+ logsigmoid.(m.logitp)
 
 logpdf(m::Geometric,     x::NGramMatrix{T})         where {T<:Sequence}            = mean(_logpdf(m, x); dims=1)
 logpdf(m::Geometric{Tm}, x::NGramMatrix{Maybe{Tx}}) where {Tm<:Real, Tx<:Sequence} = mean(coalesce.(_logpdf(m, x), Tm(0e0)); dims=1)
+logpdf(m::Geometric, x::SparseMatrixCSC)                                           = mean(_logpdf(m, x); dims=1)
 
 ####
     #   Functions for generating random samples
 ####
 
-Base.rand(m::Geometric, n::Int) = floor.(Int, log.(rand(length(m), n)) ./ logsigmoid.(-m.logitp))
+Base.rand(m::Geometric, n::Int) = ArrayNode(SparseMatrixCSC(floor.(Int, log.(rand(length(m), n)) ./ logsigmoid.(-m.logitp))))
 Base.rand(m::Geometric) = rand(m, 1)
 
 ####
