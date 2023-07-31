6777a944d2c93cf2c54837949ecaaf882e6d950f
Martin Rektoris <martin.rektoris@gmail.com>
2023-07-31 16:41:34 +0200

    Fix PN unit test

--- test/productnode.jl
+++ test/productnode.jl
@@ -4,12 +4,14 @@ import Mill
 @testset "ProductNode --- forward" begin
     d1 = 9
     d2 = 11
-    n = 15
-    x = randn(d1 + d2, n)
-    m = ProductNode((SumProductSet.MvNormal(d1), SumProductSet.MvNormal(d2)))
+    nobs = 100
+    x = randn(Float32, d1 + d2, nobs)
+    m = ProductNode([SumProductSet.MvNormal(d1), SumProductSet.MvNormal(d2)], [1:d1, d1+1:d1+d2])
 
     @test !isnothing(SumProductSet.logpdf(m, x))
-    @test SumProductSet.logpdf(m, x) ≈ SumProductSet.logpdf(m[1], x[1:d1, :]) + SumProductSet.logpdf(m[2], x[d1+1:d1+d2, :])
+    @test length(SumProductSet.logpdf(m, x)) == nobs
+    @test SumProductSet.logpdf(m, x) ≈ SumProductSet.logpdf(m.components[1], x[1:d1, :]) + 
+                                       SumProductSet.logpdf(m.components[2], x[d1+1:d1+d2, :])
 end
 
 
@@ -20,10 +22,10 @@ end
 @testset "ProductNode --- integration with Flux" begin
     d1 = 9
     d2 = 11
-    n = 15
-    x = randn(d1 + d2, n)
+    nobs = 100
+    x = randn(Float32, d1 + d2, nobs)
 
-    m = ProductNode((SumProductSet.MvNormal(d1), SumProductSet.MvNormal(d2)))
+    m = ProductNode([SumProductSet.MvNormal(d1), SumProductSet.MvNormal(d2)])
     ps = Flux.params(m)
 
     @test !isempty(ps)
@@ -34,20 +36,24 @@ end
     # Integration with Mill.ArrayNode
     d1 = 9
     d2 = 11
-    n = 15
-    x = Mill.ArrayNode(randn(d1 + d2, n))
-    m = ProductNode((SumProductSet.MvNormal(d1), SumProductSet.MvNormal(d2)))
+    nobs = 100
+    x = Mill.ArrayNode(randn(Float32, d1 + d2, nobs))
+    m = ProductNode([SumProductSet.MvNormal(d1), SumProductSet.MvNormal(d2)], [1:d1, d1+1:d1+d2])
 
     @test !isnothing(SumProductSet.logpdf(m, x))
-    @test SumProductSet.logpdf(m, x) ≈ SumProductSet.logpdf(m[1], x.data[1:d1, :]) + SumProductSet.logpdf(m[2], x.data[d1+1:d1+d2, :])
+    @test length(SumProductSet.logpdf(m, x)) == nobs
+    @test SumProductSet.logpdf(m, x) ≈ SumProductSet.logpdf(m.components[1], x.data[1:d1, :]) + 
+                                       SumProductSet.logpdf(m.components[2], x.data[d1+1:d1+d2, :])
 
-    # Integration with Mill.ProductNode
+    # Integration with Mill.ProductNode{<:NamedTuple}
     d1 = 9
     d2 = 11
-    n = 15
-    x = Mill.ProductNode((randn(d1, n), randn(d2, n)))
-    m = ProductNode((SumProductSet.MvNormal(d1), SumProductSet.MvNormal(d2)))
+    nobs = 100
+    x = Mill.ProductNode(a=randn(Float32, d1, nobs), b=randn(Float32, d2, nobs))
+    m = ProductNode(a=SumProductSet.MvNormal(d1), b=SumProductSet.MvNormal(d2))
     @test !isnothing(SumProductSet.logpdf(m, x))
-    @test SumProductSet.logpdf(m, x) ≈ SumProductSet.logpdf(m[1], x.data[1]) + SumProductSet.logpdf(m[2], x.data[2])
+    @test length(SumProductSet.logpdf(m, x)) == nobs
+    @test SumProductSet.logpdf(m, x) ≈ SumProductSet.logpdf(m.components[1], x.data[:a]) + 
+                                       SumProductSet.logpdf(m.components[2], x.data[:b])
 
 end
