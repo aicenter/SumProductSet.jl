27e0303a31a3e3619b0a708294b33cc691b9b632
Martin Rektoris <martin.rektoris@gmail.com>
2023-08-01 10:30:41 +0200

    Raname loss fns

--- src/loss.jl
+++ src/loss.jl
@@ -1,21 +1,19 @@
 
-function ul_loss(m::SumNode, xu)
+function em_loss(m::SumNode, xu)
     # E-step for unlabeled data
     p = []
     Flux.Zygote.ignore() do 
         p = softmax(logjnt(m, xu); dims=1)
     end
+    # M-step objective
     -mean(p .* logjnt(m, xu))
 end 
 
-# inefficient implementation of supervised loss
-function sl_loss(m::SumNode, xl, yl::Vector{Int})
-    -mean(logjnt(m, xl)[CartesianIndex.(yl, 1:length(yl))])
-end
+ce_loss(m::SumNode, xl, yl::Vector{Int}) = -mean(logjnt(m, xl)[CartesianIndex.(yl, 1:length(yl))])
 
 function ssl_loss(m::SumNode, xu, xl, yl::Vector{Int})
     nu = nobs(xu) 
     nl = nobs(xl)
 
-    (nl*sl_loss(m, xl, yl) + nu*ul_loss(m, xu))/(nu+nl)
+    (nl*ce_loss(m, xl, yl) + nu*em_loss(m, xu))/(nu+nl)
 end 
