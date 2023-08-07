1d40e547f5b99aaffffc172c4aa1a875b68866b1
Martin Rektoris <martin.rektoris@gmail.com>
2023-08-07 18:23:32 +0200

    Update setmixture modelbilders

--- src/modelbuilders.jl
+++ src/modelbuilders.jl
@@ -10,7 +10,7 @@ end
     spn(d::Int, l::Int, n::Int)
     spn(d::Int, n::Vector{Int})
 
-Implement SumProduct Network for vector data with binary splitting ProductNodes.
+Implement SumProduct Network for vector data with binary splits at ProductNodes.
 
 # Arguments
 - `d::Int`: the dimensionality of feature vector.
@@ -66,30 +66,92 @@ function spn(d::Int, n::Vector{Int}; leaf::Function=d->MvNormal(d))
     length(sum_comp) == 1 ? first(sum_comp) : SumNode(sum_comp)
 end
 
-function setmixture(nb::Int, ni::Int, d::Int; cdist::Function=()->Poisson(),
-    dtype::Type{<:Real}=Float64, minit::Symbol=:uniform, sinit::Symbol=:unit, stype::Symbol=:full, r::Real=0.)
+"""
+    setmixture(nb::Int, ni::Int, d::Int; kwargs...)
 
-    ps = (; dtype, minit, sinit, stype, r)
-    fdist() = ni > 1 ? gmm(ni, d; ps...) : MvNormal(d; ps...)
+Implement a constructor for mixture of `SetNode`s with specifed feature and cardinality distributions.
+
+# Arguments
+- `nb::Int`: the number of `SetNode` components in the root mixture.
+- `ni::Int`: the number of `Leaf` components in the `SetNodes`'s feature mixture. 
+- `d::Int`: the dimensionality of the `Leaf` distribution.
+# Examples
+```julia-repl
+julia> setmixture(2, 3, 4)
+SumNode
+  ├── SetNode
+  │     ├── c: Poisson
+  │     ╰── f: SumNode
+  │              ├── MvNormal
+  │              ├── MvNormal
+  │              ╰── MvNormal
+  ╰── SetNode
+        ├── c: Poisson
+        ╰── f: SumNode
+                 ├── MvNormal
+                 ├── MvNormal
+                 ╰── MvNormal
+```
+
+"""
+function setmixture(nb::Int, ni::Int, d::Int; 
+    leaf::Function=d->MvNormal(d), card_dist::Function=()->Poisson(), dtype::Type{<:Real}=Float32)
+
+    feat_dist() = ni > 1 ? SumNode([leaf(d) for _ in 1:ni]; dtype=dtype) : leaf(d)
     
-    components = map(1:nb) do _
-        pc = cdist()
-        pf = fdist()
+    set_dists = map(1:nb) do _
+        pc = card_dist()
+        pf = feat_dist()
         SetNode(pf, pc)
     end
-    SumNode(components; dtype=dtype)
+    SumNode(set_dists; dtype=dtype)
 end
 
-function sharedsetmixture(nb::Int, nis::Int, nin::Int, d::Int; cdist::Function=()->Poisson(), 
-    dtype::Type{<:Real}=Float64, minit::Symbol=:uniform, sinit::Symbol=:unit, stype::Symbol=:full, r::Real=0.)
-    ps = (; dtype, minit, sinit, stype, r)
+"""
+    sharedsetmixture(nb::Int, nis::Int, nin::Int, d::Int; kwargs...)
 
-    sharedcomps = [MvNormal(d; ps...) for _ in 1:nis]
-    bagcomps = map(1:nb) do _
-        pc = cdist()
-        nonsharedcomps = [MvNormal(d; ps...) for _ in 1:nin]
-        pf = SumNode([nonsharedcomps; sharedcomps]; dtype)
+Implement a constructor for mixture of `SetNode`s with specifed feature and cardinality distributions.
+Some of feature components are shared according to chosen constructor parameters.
+
+# Arguments
+- `nb::Int`: the number of `SetNode` components in the root mixture.
+- `nis::Int`: the number of `Leaf` components in the `SetNodes`'s feature mixture that are
+shared with `Leaf` components among all other `SetNode`s on the same hierarchy level. 
+- `nis::Int`: the number of `Leaf` components in the `SetNodes`'s feature mixture that are
+`not` shared with `Leaf` components among all other `SetNode`s on the same hierarchy level. 
+- `d::Int`: the dimensionality of the `Leaf` distribution.
+# Examples
+```julia-repl
+julia> sharedsetmixture(2, 1, 1, 3)
+SumNode
+  ├── SetNode
+  │     ├── c: Poisson
+  │     ╰── f: SumNode
+  │              ├── MvNormal
+  │              ╰── MvNormal
+  ╰── SetNode
+        ├── c: Poisson
+        ╰── f: SumNode
+                 ├── MvNormal
+                 ╰── MvNormal
+
+julia> m.components[2].feature.components[1] == m.components[1].feature.components[1]
+false
+
+julia> m.components[2].feature.components[2] == m.components[1].feature.components[2]
+true
+```
+
+"""
+function sharedsetmixture(nb::Int, nis::Int, nin::Int, d::Int; 
+    leaf::Function=d->MvNormal(d), card_dist::Function=()->Poisson(), dtype::Type{<:Real}=Float32)
+
+    scomponents = [leaf(d) for _ in 1:nis]
+    set_dists = map(1:nb) do _
+        pc = card_dist()
+        nscomponents = [leaf(d) for _ in 1:nin]
+        pf = SumNode([nscomponents; scomponents]; dtype)
         SetNode(pf, pc)
     end
-    SumNode(bagcomps; dtype)
+    SumNode(set_dists; dtype)
 end
