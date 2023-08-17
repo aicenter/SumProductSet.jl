a2c456366530ead577a34aaf17bf9fc466101dfe
Martin Rektoris <martin.rektoris@gmail.com>
2023-08-17 15:45:58 +0200

    Fix distributions due to clean up

--- src/distributions/poisson.jl
+++ src/distributions/poisson.jl
@@ -32,25 +32,23 @@ end
 Flux.@functor Poisson
 
 Poisson(lograte::AbstractFloat) = Poisson([lograte])
-Poisson(n::Int) = Poisson(Float32.(log.(rand(2:10, n))))
+Poisson(n::Int) = Poisson(Float64.(log.(rand(2:10, n)))) # pois_rand does not work with Float64
 Poisson() = Poisson(1)
 
 ####
 #   Functions for calculating the likelihood
 ####
-
+_logfactorial(x; dtype::Type{<:Real}=Float64) = sum(log.(2:x))
 _logpdf(lograte, x) = x .* lograte .- exp.(lograte) .- _logfactorial.(x)
 logpdf(m::Poisson, x::Matrix{<:Real}) = sum(_logpdf(m.lograte, x), dims=1)
 logpdf(m::Poisson, x::Vector{<:Real}) = sum(_logpdf(m.lograte, hcat(x...)), dims=1)
 logpdf(m::Poisson, x::Real) = hcat(_logpdf(m.lograte, x))  # for consistency
-logpdf(m::Poisson, x::SparseMatrixCSC) = sum(_logpdf(m.lograte, x), dims=1)
 
 ####
 #   Functions for generating random samples
 ####
 
-# pois_rand does not support Float64
-Base.rand(m::Poisson, n::Int) = Mill.ArrayNode([pois_rand(exp(logr)) for logr in Float64.(m.lograte), _ in 1:n])
+Base.rand(m::Poisson, n::Int) = Mill.ArrayNode([pois_rand(exp(logr)) for logr in m.lograte, _ in 1:n])
 Base.rand(m::Poisson) = rand(m, 1)
 
 ####
