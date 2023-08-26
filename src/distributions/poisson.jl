7a8ef4692aa691cb359ca6aceabe7552a2476c76
Martin Rektoris <martin.rektoris@gmail.com>
2023-08-26 22:51:16 +0200

    Add support for SparseArrays to Poisson

--- src/distributions/poisson.jl
+++ src/distributions/poisson.jl
@@ -32,8 +32,8 @@ end
 Flux.@functor Poisson
 
 Poisson(lograte::AbstractFloat) = Poisson([lograte])
-Poisson(n::Int) = Poisson(Float32.(log.(rand(2:10, n))))
-Poisson() = Poisson(1)
+Poisson(n::Int; dtype::Type{<:Real}=Float32) = Poisson(dtype.(log.(rand(2:10, n))))
+Poisson(dtype::Type{<:Real}=Float32) = Poisson(1; dtype=dtype)
 
 ####
 #   Functions for calculating the likelihood
@@ -44,7 +44,7 @@ function _logpdf_poisson(lograte::Vector{T}, x::Matrix{<:Real}) where {T<:Real}
     linit = -sum(exp, lograte)
 
     l = fill(linit, 1, nobs)
-    for j in 1:nobs
+    @inbounds for j in 1:nobs
         for i in 1:ndims
             l[j] += x[i, j] * lograte[i] - logfactorial(x[i, j])
         end
@@ -52,7 +52,24 @@ function _logpdf_poisson(lograte::Vector{T}, x::Matrix{<:Real}) where {T<:Real}
     l
 end
 
-function _logpdf_poisson_back(lograte::Vector{T}, x, Δy) where {T<:Real}
+
+function _logpdf_poisson(lograte::Vector{T}, x::SparseMatrixCSC) where {T<:Real}
+    linit = -sum(exp, lograte)
+    l = fill(linit, 1, size(x, 2))
+
+    rows = rowvals(x)
+    vals = nonzeros(x)
+    @inbounds for j in 1:size(x, 2)
+        for i in nzrange(x, j)
+            row = rows[i]
+            val = vals[i]
+            l[j] += val * lograte[row] - logfactorial(val)
+        end
+    end
+    l
+end
+
+function _logpdf_poisson_back(lograte::Vector{T}, x::Matrix{<:Real}, Δy) where {T<:Real}
     ndims, nobs = size(x)
     sum_Δy = sum(Δy)
     Δp = zeros(T, ndims)
@@ -66,6 +83,28 @@ function _logpdf_poisson_back(lograte::Vector{T}, x, Δy) where {T<:Real}
     Δp, NoTangent()
 end
 
+function _logpdf_poisson_back(lograte::Vector{T}, x::SparseMatrixCSC, Δy) where {T<:Real}
+    ndims, nobs = size(x)
+    sum_Δy = sum(Δy)
+    Δp = zeros(T, ndims)
+
+    @inbounds for i in eachindex(lograte)
+        Δp[i] -= sum_Δy * exp(lograte[i])
+    end
+
+    rows = rowvals(x)
+    vals = nonzeros(x)
+    @inbounds for j in 1:size(x, 2)
+        for i in nzrange(x, j)
+            row = rows[i]
+            val = vals[i]
+            Δp[row] += val * Δy[j]
+        end
+    end
+    
+    Δp, NoTangent()
+end
+
 
 function ChainRulesCore.rrule(::typeof(_logpdf_poisson), args...)
     _logpdf_pullback = Δy -> (NoTangent(), _logpdf_poisson_back(args..., Δy)...)
@@ -73,6 +112,7 @@ function ChainRulesCore.rrule(::typeof(_logpdf_poisson), args...)
 end
 
 logpdf(m::Poisson, x::Matrix{<:Real}) = _logpdf_poisson(m.lograte, x)
+logpdf(m::Poisson, x::SparseMatrixCSC) = _logpdf_poisson(m.lograte, x)
 logpdf(m::Poisson, x::Union{T, Vector{T}} where T<:Real) = logpdf(m, hcat(x...))
 
 # old logpdfs
