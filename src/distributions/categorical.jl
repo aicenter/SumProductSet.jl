725088a462767e5d78d172680c2fb5114adf5176
Martin Rektoris <martin.rektoris@gmail.com>
2023-09-07 21:42:00 +0200

    Optimize Categorical distribution

--- src/distributions/categorical.jl
+++ src/distributions/categorical.jl
@@ -36,23 +36,8 @@ Categorical(n::Integer; dtype::Type{<:Real}=Float32) = Categorical(ones(dtype, n
 #   Functions for calculating the likelihood
 ####
 
-# function logpdf(m::Categorical, x::Union{Int, Vector{Int}})
-#     logp = logsoftmax(m.logp)
-#     logp[x]
-# end
-
-# logpdf(m::Categorical, x::Matrix)         = logpdf(m, vec(x))
-# logpdf(m::Categorical, x::Real)           = logpdf(m, convert.(Int64, x))
-# logpdf(m::Categorical, x::Vector{<:Real}) = logpdf(m, convert.(Int64, x))
-# logpdf(m::Categorical, x::Matrix{<:Real}) = logpdf(m, convert.(Int64, vec(x)))
-
-# _logpdf(m::Categorical, x::OneHotArray)   =                    reshape(logsoftmax(m.logp), 1, :)  * x
-# _logpdf(m::Categorical, x::MaybeHotArray) = PostImputingMatrix(reshape(logsoftmax(m.logp), 1, :)) * x
-
-# logpdf(m::Categorical, x::Union{OneHotArray, MaybeHotArray}) = _logpdf(m, x)
-
-
 logpdf(m::Categorical, x::Union{OneHotArray, MaybeHotArray}) = _logpdf_cat(m.logp, x)
+lgpdf(m::Categorical{T}, x::MaybeHotMatrix{Missing}) where {T<:Real} = zeros(T, 1, size(x, 2))
 
 _get_inidices(x::OneHotArray) = x.indices
 _get_inidices(x::MaybeHotMatrix) = x.I
