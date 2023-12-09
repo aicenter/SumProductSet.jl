1697ec295ab0be50d4fcee9075e20b4b6a492dcf
Martin Rektoris <martin.rektoris@gmail.com>
2023-12-09 12:58:27 +0100

    Update SPSN CE loss

--- src/loss.jl
+++ src/loss.jl
@@ -9,11 +9,8 @@ function em_loss(m::SumNode, xu)
     -mean(p .* logjnt(m, xu))
 end 
 
-ce_loss(m::SumNode, xl, yl::Vector{Int}) = -mean(logjnt(m, xl)[CartesianIndex.(yl, 1:length(yl))])
 
-function ssl_loss(m::SumNode, xu, xl, yl::Vector{Int})
-    nu = nobs(xu) 
-    nl = nobs(xl)
+# disc loss is actually a cross entropy loss  
 
-    (nl*ce_loss(m, xl, yl) + nu*em_loss(m, xu))/(nu+nl)
-end 
+disc_loss(m::SumNode, xl, yl::Vector{Int}) = -mean(logsoftmax(logjnt(m, xl), dims=1)[CartesianIndex.(yl, 1:length(yl))])
+gen_loss(m::SumNode, xl, yl::Vector{Int}) = -mean(logjnt(m, xl)[CartesianIndex.(yl, 1:length(yl))])
